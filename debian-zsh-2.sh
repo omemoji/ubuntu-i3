451d033606e9a0f7748ba515be88d2bb7e8ab0e2
omemoji <omemoji1111@gmail.com>
2021-11-23 20:28:56 +0900

    change ln

--- debian-zsh-2.sh
+++ debian-zsh-2.sh
@@ -13,7 +13,13 @@ set -u
 for f in .??*; do
     [ "$f" = ".git" ] && continue
     [ "$f" = ".gitconfig.local.template" ] && continue
+    [ "$f" = ".Xauthority" ] && continue
     [ "$f" = ".gitmodules" ] && continue
+    [ "$f" = ".zsh_history" ] && continue
+    [ "$f" = ".fehbg" ] && continue
+    [ "$f" = ".bashrc" ] && continue
+    [ "$f" = ".bash_history" ] && continue
+    [ "$f" = ".bashrc" ] && continue
     ln -snfv ~/dotfiles/"$f" ~/
 done
 #config
@@ -25,7 +31,8 @@ cd ..
 #vscode 
 code
 echo "待機中..."
-sleep 10;killall code
+sleep 6;killall code
+echo "fs.inotify.max_user_watches=524288" | sudo tee /etc/sysctl.conf
 rm -r ~/.config/Code/User/settings.json
 ln -snfv ~/dotfiles/settings.json ~/.config/Code/User/
 rm ~/tmp.txt
