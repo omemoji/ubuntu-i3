e116c1e20bcee65319f4e3632d6baf49ecf47a27
omemoji <omemoji1111@gmail.com>
2021-12-02 16:39:30 +0900

    add jquake in alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,3 @@
 alias gmount='rclone mount gdrive:/ ~/googledrive'
-alias android= "sh /usr/local/android-studio/bin/studio.sh"
+alias android="sh /usr/local/android-studio/bin/studio.sh"
+alias jquake="cd /home/omemoji/Downloads/zip/JQuake_1.6.2_linux/ && java -jar JQuake.jar -Xmx200m -Xms32m -Xmn2m -Djava.net.preferIPv4Stack=true"
