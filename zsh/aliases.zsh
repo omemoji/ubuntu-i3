43c61aa434dad0c9ae88538b6aa9ce046b5d044c
omemoji <omemoji1111@gmail.com>
2022-01-25 13:27:09 +0900

    c

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,5 @@
 alias gmount='rclone mount gdrive:/ ~/googledrive'
 alias android="sh /usr/local/android-studio/bin/studio.sh"
 alias jquake="cd /home/omemoji/Downloads/zip/JQuake_1.6.2_linux/ && java -jar JQuake.jar -Xmx200m -Xms32m -Xmn2m -Djava.net.preferIPv4Stack=true"
+alias startwacom_16='xsetwacom --set 16 MapToOutput "DisplayPort-0"'
+alias startwacom_10='xsetwacom --set 10 MapToOutput "DisplayPort-0"'
