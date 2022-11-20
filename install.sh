0473521e535e56534549e504d7d4062b8f5ac521
omemoji <omemoji1111@gmail.com>
2022-11-21 04:35:18 +0900

    add polybar

--- install.sh
+++ install.sh
@@ -59,7 +59,7 @@ libspa-0.2-bluetooth libspa-0.2-jack \
 pipewire-audio-client-libraries \
 #LDAC Bluetooth ABR library (shared library)
 #LDAC Bluetooth encoder library (shared library)
-yes | sudo apt install libldacbt-{abr,enc}2
+yes | sudo apt install libldacbt-abr2 libldacbt-enc2
 yes | sudo apt install wireplumber pipewire-media-session-
 systemctl --user --now enable wireplumber.service
 yes | sudo apt install --no-install-recommends pavucontrol
